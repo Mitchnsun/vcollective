7b5a2bfd96e57550cae7401bb75e72ebeca3a84b
Matthieu Compérat <matthieu.comperat@gmail.com>
2020-10-20 11:54:43 +0200

    Reverse brand Louis Vuitton

--- utils/obfuscator.js
+++ utils/obfuscator.js
@@ -1,2 +1,2 @@
 const BRAND_TO_OBFUSC = ['Louis Vuitton'];
-export const brand = (brand) => BRAND_TO_OBFUSC.indexOf(brand) != -1 ? '-' : brand;
+export const brand = (brand) => BRAND_TO_OBFUSC.indexOf(brand) != -1 ? brand.split("").reverse().join("") : brand;
